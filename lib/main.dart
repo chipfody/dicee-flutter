cbfce09f3096fa96846bfdf3f3e165844a777032
chipfody <60615532+chipfody@users.noreply.github.com>
2020-07-10 11:31:56 -0400

    Added Randomizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -21,7 +22,7 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 5;
+  int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
   @override
@@ -33,8 +34,8 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = 6;
-                  print('diceNumber = $leftDiceNumber');
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  print('left diceNumber = $leftDiceNumber');
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -43,9 +44,12 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right Button got pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                  print('right diceNumber = $rightDiceNumber');
+                });
               },
-              child: Image.asset('images/dice3.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
